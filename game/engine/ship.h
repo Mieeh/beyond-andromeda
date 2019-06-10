4674164fb3193abd1710da36e817af00885be2f0
David Styrbjörn <davidstyrbjorn@gmail.com>
2019-06-10 13:53:41 +0200

    jdwlak

--- game/engine/ship.h
+++ game/engine/ship.h
@@ -1,5 +1,21 @@
 #pragma once
 
-struct Ship {
+#include<string>
+#include<vector>
 
+struct ShipStructure {
+	int hull;      // Some lvalue used when evalutaing hull related calcs for the ship
+	int analyzer;  // Some lvalue used when evaluating things related to analyzing when using the ship
+	int engine;    // Some lvalue used when evaluating engine related things for the ship
+};
+
+struct ShipInventory {
+	std::vector<int> element_list;
+};
+
+// Main struct for a ship, this is serialized 
+struct Ship {
+	int hp; // Call this integrity in-game, it's a value for how damaged the ship is
+	ShipStructure shipStructure;
+	ShipInventory shipInventory;
 };
